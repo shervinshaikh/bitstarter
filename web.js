c33808e8f77d107ba125ede58dcfeba4a925812e
Shervin Shaikh <shervinater@gmail.com>
2013-07-15 21:40:04 -0700

    fixed typo

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 var index = 'index.html'
-var content = fs.readFileSynce(index)
+var content = fs.readFileSync(index)
 
 app.get('/', function(request, response) {
   response.send(content.toString('utf-8'));
